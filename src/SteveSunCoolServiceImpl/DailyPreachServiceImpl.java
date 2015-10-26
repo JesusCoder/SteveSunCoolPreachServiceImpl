4fbb61c693363169459468747072aceb65af03aa
Steve Sun <jiahuan@amazon.com>
2015-10-26 07:24:45 -0700

    added SteveSunCoolPreachServiceImpl project

---
+++ src/SteveSunCoolServiceImpl/DailyPreachServiceImpl.java
@@ -0,0 +1,28 @@
+package SteveSunCoolServiceImpl;
+
+import SteveSunPreachService.DailyPreachService;
+
+public class DailyPreachServiceImpl implements DailyPreachService{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public DailyPreachServiceImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void dailyStudyBible() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void doDailyFellowship() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
